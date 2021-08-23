024bc9dbbc9a7fa6a3c9fce3056f54c93fb5ae4f
brandonlroberts <brandonlroberts@outlook.com>
2021-08-22 21:49:50 -0400

    Added routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
