6ed3370247a21aed00fd34ddb511834908c76d18
brandonlroberts <brandonlroberts@outlook.com>
2021-08-22 22:04:43 -0400

    Working on routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'movies', component: MoviesComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 @NgModule({
